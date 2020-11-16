679d0ff72393a990c67239f1f84d03ea5ceed58c
charles66820 <charles.goedefroit@gmail.com>
2020-11-16 23:26:50 +0100

    try to change context for scope

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,26 +1,24 @@
-(() => {
-  if ("serviceWorker" in navigator)
-    navigator.serviceWorker.register("/js/sw.js");
+if ("serviceWorker" in navigator)
+  navigator.serviceWorker.register("/js/sw.js");
 
-  // Show install button and prepare install prompt
-  window.addEventListener("beforeinstallprompt", e => {
-    e.preventDefault();
-    let btnInstall = document.querySelector("#btnInstall");
-    btnInstall.style.display = "block";
+// Show install button and prepare install prompt
+window.addEventListener("beforeinstallprompt", e => {
+  e.preventDefault();
+  let btnInstall = document.querySelector("#btnInstall");
+  btnInstall.style.display = "block";
 
-    // Show the PWA install prompt
-    btnInstall.addEventListener("click", () => {
-      btnInstall.style.display = "none";
-      e.prompt();
-      e.userChoice.then(choiceResult => {
-        if (choiceResult.outcome === "accepted")
-          console.log("PWD install");
-      });
+  // Show the PWA install prompt
+  btnInstall.addEventListener("click", () => {
+    btnInstall.style.display = "none";
+    e.prompt();
+    e.userChoice.then(choiceResult => {
+      if (choiceResult.outcome === "accepted")
+        console.log("PWD install");
     });
   });
+});
 
-  // Show messege after PWA install
-  window.addEventListener("appinstalled", () => {
-    alert("Thanks for install PWA");
-  });
-})();
+// Show messege after PWA install
+window.addEventListener("appinstalled", () => {
+  alert("Thanks for install PWA");
+});
