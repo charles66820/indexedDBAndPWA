778bbbe942cc31d1a94263d427d127c6f5ec90b2
charles66820 <charles.goedefroit@gmail.com>
2020-11-18 22:58:26 +0100

    start test with db

--- src/public/sw.js
+++ src/public/sw.js
@@ -24,9 +24,41 @@ self.addEventListener("activate", e => {
 });
 */
 
+let db = null;
+openDb();
+
 self.addEventListener("fetch", e => {
+  // Intercept /api/notes request
+  let notesOS = db.transaction("notes", "readwrite").objectStore("notes");
+
+  console.log(e.request);
+  // if fetch we can clone response and put it in cache
+  // GET /api/notes
+  //notes.forEach(note => notesOS.add(note));
+  // POST /api/notes
+  // DELETE /api/notes/:id
+
+
+  // If request is in cache returns cache else send request
   e.respondWith(caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-      // if fetch we can clone response and put it in cache
-    }));
+    return res || fetch(e.request);
+  }));
 });
+
+function openDb() {
+  let dbRequest = indexedDB.open("noteOfflineDb", 1);
+  dbRequest.addEventListener("error", e => console.error("Error on open noteOfflineDb: " + e));
+  dbRequest.addEventListener("upgradeneeded", e => {
+    db = e.target.result;
+
+    // Create db schema
+    let notesOS = db.createObjectStore("notes"); // , { keyPath: "id", autoIncrement: true }
+    notesOS.createIndex("id", "id", { unique: false });
+    notesOS.createIndex("title", "title", { unique: false });
+    notesOS.createIndex("description", "description", { unique: false });
+    notesOS.createIndex("sync", "scync", { unique: false });
+
+    db.transaction.addEventListener("complete", () => console.info("All donne!"));
+    db.transaction.addEventListener("error", e => console.error("Error on transaction: " + e));
+  });
+}
