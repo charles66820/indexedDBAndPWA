08947bbdf99ca89afb8fdeb7debc35e0dbdc3e32
charles66820 <charles.goedefroit@gmail.com>
2020-11-24 00:39:03 +0100

    aync test

--- src/public/sw.js
+++ src/public/sw.js
@@ -2,26 +2,28 @@
 importScripts("/js/cache-polyfill.js"); // For support multiple browser
 
 // Init db
-function openDb(callback) {
-  let dbRequest = indexedDB.open("noteOfflineDb", 1);
-  dbRequest.addEventListener("error", e => console.error("Error on open noteOfflineDb: " + e));
-  return dbRequest.addEventListener("upgradeneeded", e => {
-    let db = e.target.result;
-    db.onclose = e => console.error("Close on db: " + e);
-    db.onerror = e => console.error("Error on db: " + e);
+function openDb() {
+  return new Promise(function(resolve, reject) {
+    let dbRequest = indexedDB.open("noteOfflineDb", 1); // reject(Error("It broke"));
+    dbRequest.addEventListener("error", e => console.error("Error on open noteOfflineDb: " + e));
+    dbRequest.addEventListener("upgradeneeded", e => {
+      let db = e.target.result;
+      db.onclose = e => console.error("Close on db: " + e);
+      db.onerror = e => console.error("Error on db: " + e);
 
-    // Create db schema
-    let notesOS = db.createObjectStore("notes"); // , { keyPath: "id", autoIncrement: true }
-    notesOS.createIndex("id", "id", { unique: false });
-    notesOS.createIndex("title", "title", { unique: false });
-    notesOS.createIndex("description", "description", { unique: false });
-    notesOS.createIndex("sync", "scync", { unique: false });
+      // Create db schema
+      let notesOS = db.createObjectStore("notes"); // , { keyPath: "id", autoIncrement: true }
+      notesOS.createIndex("id", "id", { unique: false });
+      notesOS.createIndex("title", "title", { unique: false });
+      notesOS.createIndex("description", "description", { unique: false });
+      notesOS.createIndex("sync", "scync", { unique: false });
 
-    if (db.transaction) {
-      db.transaction.oncomplete = () => console.info("All donne!");
-      db.transaction.onerror = e => console.error("Error on transaction: " + e);
-    }
-    return callback(db);
+      if (db.transaction) {
+        db.transaction.oncomplete = () => console.info("All donne!");
+        db.transaction.onerror = e => console.error("Error on transaction: " + e);
+      }
+      return resolve(db);
+    });
   });
 }
 
@@ -53,51 +55,50 @@ self.addEventListener("activate", e => {
 self.addEventListener("fetch", async e => {
   let pathname = new URL(e.request.url).pathname;
   // Intercept /api/notes request and put it in cache
-  return openDb(db => {
-    if (db) {
-      if (e.request.method == "GET" && pathname == "/api/notes") {
-        function getAllFromDb() {
-          return notesOS.getAll().addEventListener("success", notes => {
-            return e.respondWith(new Response(JSON.stringify(notes), { "status": 200, "statusText": "OK from indexedDB" }));
-          }).addEventListener("error", err => {
-            return e.respondWith(new Response(JSON.stringify(err), { "status": 500, "statusText": "Error form Web worker" }));
-          });
-        }
-        console.log(db);
-        let notesOS = db.transaction.objectStore("notes"); //("notes", "readwrite")
-        if (navigator.onLine) {
-          return fetch(e.request).then(res => res.json())
-            .then(res => {
-              res.forEach(i => notesOS.add({
-                id: i.id,
-                title: i.title,
-                description: i.description,
-                sync: true,
-                delete: false
-              }));
-              return res;
-            }).catch(err => {
-              console.error(err);
-              return getAllFromDb();
-            });
-        } else {
-          return getAllFromDb();
-        }
-      } else if (e.request.method == "POST" && pathname == "/api/notes") {
-
-      } else if (e.request.method == "DELETE" && pathname == "/api/notes/:id") {
-
-      } else {
-        // If request is in cache returns cache else send request
-        return e.respondWith(caches.match(e.request).then(res => { // For pwa
-          return res || fetch(e.request);
-        }));
+  let db = await openDb().then( db => db);
+  if (db) {
+    if (e.request.method == "GET" && pathname == "/api/notes") {
+      function getAllFromDb() {
+        return notesOS.getAll().addEventListener("success", notes => {
+          return e.respondWith(new Response(JSON.stringify(notes), { "status": 200, "statusText": "OK from indexedDB" }));
+        }).addEventListener("error", err => {
+          return e.respondWith(new Response(JSON.stringify(err), { "status": 500, "statusText": "Error form Web worker" }));
+        });
       }
+      console.log(db);
+      let notesOS = db.transaction.objectStore("notes"); //("notes", "readwrite")
+      if (navigator.onLine) {
+        return fetch(e.request).then(res => res.json())
+          .then(res => {
+            res.forEach(i => notesOS.add({
+              id: i.id,
+              title: i.title,
+              description: i.description,
+              sync: true,
+              delete: false
+            }));
+            return res;
+          }).catch(err => {
+            console.error(err);
+            return getAllFromDb();
+          });
+      } else {
+        return getAllFromDb();
+      }
+    } else if (e.request.method == "POST" && pathname == "/api/notes") {
+
+    } else if (e.request.method == "DELETE" && pathname == "/api/notes/:id") {
+
     } else {
       // If request is in cache returns cache else send request
       return e.respondWith(caches.match(e.request).then(res => { // For pwa
         return res || fetch(e.request);
       }));
     }
-  });
+  }
+
+  // If request is in cache returns cache else send request
+  return e.respondWith(caches.match(e.request).then(res => { // For pwa
+    return res || fetch(e.request);
+  }));
 });
